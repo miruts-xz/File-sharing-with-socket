19fcef058134fb60bd9961ef270aa5d203cd1f12
akuadane <akuadane1205@gmail.com>
2021-05-30 17:52:12 +0300

    Added username and password

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -14,29 +15,40 @@ import (
 
 var fileNameToLocation map[string]string
 var listFiles []string
+var loggedIn bool = false
 
-const BUFFERSIZE = 1024
+const LOG_IN = "LOG_IN"
+const SIGN_UP = "SIGN_UP"
+const INDEX = "INDEX"
+const LIST_ALL = "LIST_ALL"
+const REQUEST_SERVER = "REQUEST_SERVER"
+const REQUEST_CLIENT = "REQUEST_CLIENT"
+const SEND_TO_SERVER = "SEND_TO_SERVER"
+const SENT_TO_CLIENT = "SEND_TO_CLIENT"
+const ERROR = "ERROR"
 
-var INDEX = "INDEX"
-var LIST_ALL = "LIST_ALL"
-var REQUEST_SERVER = "REQUEST_SERVER"
-var REQUEST_CLIENT = "REQUEST_CLIENT"
-var SEND_TO_SERVER = "SEND_TO_SERVER"
-var SENT_TO_CLIENT = "SEND_TO_CLIENT"
-var ERROR = "ERROR"
+const FLAG_SEPARATOR = "@"
+const JSON_FILE_PATHS = "filePaths.json"
 
 func incomingHandler(sock net.Conn) {
 
 	for {
 		serverMsg, _ := bufio.NewReader(sock).ReadString('\n')
-		splitMessage := strings.Split(serverMsg, "@")
+		splitMessage := strings.Split(serverMsg, FLAG_SEPARATOR)
 		header := splitMessage[0]
 
-		if header == REQUEST_CLIENT {
+		if header == LOG_IN {
+
+			fmt.Println("\nLog in successful!")
+			loggedIn = true
+
+		} else if header == SIGN_UP {
+			fmt.Println("\nSign up successful!")
+			loggedIn = true
+		} else if header == REQUEST_CLIENT {
 
 			fileName := splitMessage[1]
 			receiverSockId := splitMessage[2]
-			fmt.Println(serverMsg)
 			receiverSockId = strings.Trim(receiverSockId, "\n")
 			fileName = strings.Trim(fileName, "\n")
 
@@ -47,7 +59,7 @@ func incomingHandler(sock net.Conn) {
 
 			if err != nil {
 				fmt.Println(err)
-				sendSocketMessage(ERROR+"@"+"File can't be found", sock)
+				sendSocketMessage(ERROR+FLAG_SEPARATOR+"File can't be found", sock)
 				continue
 			}
 
@@ -56,7 +68,7 @@ func incomingHandler(sock net.Conn) {
 			strBytes = strings.Trim(strBytes, "[")
 			strBytes = strings.Trim(strBytes, " ")
 
-			sendSocketMessage(SEND_TO_SERVER+"@"+receiverSockId+"@"+fileName+"@"+strBytes, sock)
+			sendSocketMessage(SEND_TO_SERVER+FLAG_SEPARATOR+receiverSockId+FLAG_SEPARATOR+fileName+FLAG_SEPARATOR+strBytes, sock)
 
 			fmt.Println("\nFile has been sent")
 
@@ -98,68 +110,134 @@ func incomingHandler(sock net.Conn) {
 func outgoingHandler(sock net.Conn) {
 
 	for {
-		fmt.Println("\n\n1. Share file\n2. Look for file\n ")
-		fmt.Print("Row choice > ")
-		choice, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		choice = strings.Trim(choice, "\n")
+		if !loggedIn {
+			promtBeforeLogin(sock)
+		} else {
+			promtAfterLogin(sock)
+		}
 
-		if choice == "1" {
-			fmt.Print("Enter File path: ")
-			filePath, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-			filePath = strings.Trim(filePath, "\n")
+	}
 
-			if _, err := os.Stat(filePath); err == nil {
+}
 
-				var fileName string
+func promtBeforeLogin(sock net.Conn) {
+	fmt.Println("\n\n1. Log in\n2. Sign up\n ")
+	fmt.Print("> ")
 
-				splitPath := strings.Split(filePath, "/")
-				if len(splitPath) == 1 {
-					fileName = splitPath[0]
+	choice, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	choice = strings.Trim(choice, "\n")
+
+	if choice == "1" {
+
+		fmt.Print("username> ")
+		username, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		fmt.Print("password> ")
+		password, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		username = strings.Trim(username, "\n")
+		password = strings.Trim(password, "\n")
+
+		sendSocketMessage(LOG_IN+FLAG_SEPARATOR+username+FLAG_SEPARATOR+password, sock)
+		time.Sleep(1 * time.Second)
+
+	} else if choice == "2" {
+		fmt.Print("username> ")
+		username, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		fmt.Print("password> ")
+		password1, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		fmt.Print("Re-enter password> ")
+		password2, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		username = strings.Trim(username, "\n")
+		password1 = strings.Trim(password1, "\n")
+		password2 = strings.Trim(password2, "\n")
+
+		if len(password1) < 4 {
+			fmt.Println("Password length should be greater than 3")
+			return
+		}
+		if password1 == password2 {
+			sendSocketMessage(SIGN_UP+FLAG_SEPARATOR+username+FLAG_SEPARATOR+password1, sock)
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Println("Passwords don't match.")
+			return
+		}
+
+	}
+
+}
+
+func promtAfterLogin(sock net.Conn) {
+
+	fmt.Println("\n\n1. Share file\n2. Look for file\n ")
+	fmt.Print("Row choice > ")
+	choice, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	choice = strings.Trim(choice, "\n")
+
+	if choice == "1" {
+		fmt.Print("Enter File path: ")
+		filePath, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		filePath = strings.Trim(filePath, "\n")
+
+		if _, err := os.Stat(filePath); err == nil {
+
+			var fileName string
+
+			splitPath := strings.Split(filePath, "/")
+			if len(splitPath) == 1 {
+				fileName = splitPath[0]
+			} else {
+				fileName = splitPath[len(splitPath)-1]
+			}
+			fileNameToLocation[fileName] = filePath
+
+			file, _ := json.MarshalIndent(fileNameToLocation, "", " ")
+			ioutil.WriteFile(JSON_FILE_PATHS, file, 0644)
+
+			sendSocketMessage(INDEX+FLAG_SEPARATOR+fileName, sock)
+			fmt.Println("File successfuly indexed.")
+		} else {
+			fmt.Println("File doesn't exist.")
+		}
+
+	} else if choice == "2" {
+
+		sendSocketMessage(LIST_ALL+FLAG_SEPARATOR, sock)
+		fmt.Println("Looking for shared files...")
+		time.Sleep(2 * time.Second)
+
+		if len(listFiles) == 0 {
+			fmt.Println("\n\nNo files shared so far")
+		} else {
+			fmt.Println("\n\n\t\t List of files")
+			fmt.Println("No.\tFileName")
+
+			for i := 0; i < len(listFiles); i++ {
+				fmt.Println(strconv.Itoa(i+1) + "\t" + listFiles[i])
+			}
+
+			fmt.Print("Enter file row > ")
+			fileIndexStr, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+			fileIndex, _ := strconv.Atoi(strings.Trim(fileIndexStr, "\n"))
+
+			if fileIndex >= 1 && fileIndex <= len(listFiles) {
+				fileName := strings.Trim(listFiles[fileIndex-1], "\n")
+
+				if fileNameToLocation[fileName] != "" {
+					fmt.Println("File is available loacally.")
 				} else {
-					fileName = splitPath[len(splitPath)-1]
+
+					sendSocketMessage(REQUEST_SERVER+FLAG_SEPARATOR+fileName, sock)
+					fmt.Println("Requesting " + fileName)
 				}
-				fileNameToLocation[fileName] = filePath
-				sendSocketMessage(INDEX+"@"+fileName, sock)
-				fmt.Println("File successfuly indexed.")
-			} else {
-				fmt.Println("File doesn't exist.")
+
 			}
-
-		} else if choice == "2" {
-
-			sendSocketMessage(LIST_ALL+"@", sock)
-			fmt.Println("Looking for shared files...")
-			time.Sleep(2 * time.Second)
-
-			if len(listFiles) == 0 {
-				fmt.Println("\n\nNo files shared so far")
-			} else {
-				fmt.Println("\n\n\t\t List of files")
-				fmt.Println("No.\tFileName")
-
-				for i := 0; i < len(listFiles); i++ {
-					fmt.Println(strconv.Itoa(i+1) + "\t" + listFiles[i])
-				}
-
-				fmt.Print("Enter file row > ")
-				fileIndexStr, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-				fileIndex, _ := strconv.Atoi(strings.Trim(fileIndexStr, "\n"))
-
-				if fileIndex >= 1 && fileIndex <= len(listFiles) {
-					fileName := strings.Trim(listFiles[fileIndex-1], "\n")
-
-					if fileNameToLocation[fileName] != "" {
-						fmt.Println("File is available loacally.")
-					} else {
-
-						sendSocketMessage(REQUEST_SERVER+"@"+fileName, sock)
-						fmt.Println("Requesting " + fileName)
-					}
-
-				}
-			}
-
 		}
 
 	}
@@ -167,9 +245,6 @@ func outgoingHandler(sock net.Conn) {
 }
 
 func sendSocketMessage(message string, sock net.Conn) {
-	// writer := bufio.NewWriter(sock)
-	// writer.WriteString(message + "\n")
-	// writer.Flush()
 
 	fmt.Fprintf(sock, message+"\n")
 
@@ -178,6 +253,15 @@ func sendSocketMessage(message string, sock net.Conn) {
 func main() {
 	fileNameToLocation = make(map[string]string)
 	listFiles = []string{}
+
+	bytes, error := ioutil.ReadFile(JSON_FILE_PATHS)
+
+	if error != nil {
+		fmt.Println("An error occured, couldn't get previously shared files.")
+	} else {
+		json.Unmarshal(bytes, &fileNameToLocation)
+	}
+
 	sock, err := net.Dial("tcp", "127.0.0.1:8000")
 
 	if err != nil {
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -9,30 +10,34 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 )
 
 const BUFFERSIZE = 1024
 
 var sockets map[string]net.Conn
-var socketToId map[net.Conn]string
-var fileNames map[string]net.Conn
-var cachedFileNames []string
+var socketToUsername map[net.Conn]string
+var fileNames map[string]string
+var userInfo map[string]string
 
-var INDEX = "INDEX"
-var LIST_ALL = "LIST_ALL"
-var REQUEST_SERVER = "REQUEST_SERVER"
-var REQUEST_CLIENT = "REQUEST_CLIENT"
-var SEND_TO_SERVER = "SEND_TO_SERVER"
-var SENT_TO_CLIENT = "SEND_TO_CLIENT"
-var ERROR = "ERROR"
+const LOG_IN = "LOG_IN"
+const SIGN_UP = "SIGN_UP"
+const INDEX = "INDEX"
+const LIST_ALL = "LIST_ALL"
+const REQUEST_SERVER = "REQUEST_SERVER"
+const REQUEST_CLIENT = "REQUEST_CLIENT"
+const SEND_TO_SERVER = "SEND_TO_SERVER"
+const SENT_TO_CLIENT = "SEND_TO_CLIENT"
+const ERROR = "ERROR"
+
+const FLAG_SEPARATOR = "@"
+const JSON_USER_INFO = "userInfo.json"
 
 func incomingHandler(sock net.Conn) {
 
 	for {
 
 		clientMsg, error := bufio.NewReader(sock).ReadString('\n')
-		splitMessage := strings.Split(clientMsg, "@")
+		splitMessage := strings.Split(clientMsg, FLAG_SEPARATOR)
 		header := splitMessage[0]
 
 		if error != nil {
@@ -40,7 +45,32 @@ func incomingHandler(sock net.Conn) {
 			return
 		}
 
-		if header == INDEX { // A client is telling the server the file it wants to share
+		if header == LOG_IN {
+			username := strings.Trim(splitMessage[1], "\n")
+			password := strings.Trim(splitMessage[2], "\n")
+
+			realPassword := userInfo[username]
+
+			if realPassword == password {
+
+				loginUser(username, sock)
+				sendSocketMessage(LOG_IN+FLAG_SEPARATOR+"Success", sock)
+			} else {
+				sendSocketMessage(ERROR+FLAG_SEPARATOR+"Incorrect username/password.", sock)
+			}
+
+		} else if header == SIGN_UP {
+			username := strings.Trim(splitMessage[1], "\n")
+			password := strings.Trim(splitMessage[2], "\n")
+
+			if userInfo[username] != "" {
+				sendSocketMessage(ERROR+FLAG_SEPARATOR+"Username already taken.", sock)
+			} else {
+				signupUser(username, password, sock)
+				sendSocketMessage(SIGN_UP+FLAG_SEPARATOR+"Success", sock)
+			}
+
+		} else if header == INDEX { // A client is telling the server the file it wants to share
 			fileName := splitMessage[1]
 			fileName = strings.Trim(fileName, "\n")
 			addFileName(fileName, sock)
@@ -52,7 +82,7 @@ func incomingHandler(sock net.Conn) {
 				files = key + "*" + files
 			}
 			files = strings.Trim(files, "*")
-			sendSocketMessage(LIST_ALL+"@"+files, sock)
+			sendSocketMessage(LIST_ALL+FLAG_SEPARATOR+files, sock)
 
 		} else if header == REQUEST_SERVER { // A client is requesting server for a file
 			fileName := splitMessage[1]
@@ -71,49 +101,61 @@ func incomingHandler(sock net.Conn) {
 			} else {
 				//File wasn't cached on server so look it up in the FilNames map
 
-				fileFound := false
+				fileOwnerUserName := fileNames[fileName]
 
-				for key := range fileNames {
+				fileOwnerSocket := sockets[fileOwnerUserName]
 
-					if fileName == key {
+				if fileOwnerSocket != nil {
 
-						fileFound = true
-						fileOwnerSocket := fileNames[key]
-						fmt.Println("Sending request to file owner")
-						sendSocketMessage(REQUEST_CLIENT+"@"+fileName+"@"+socketToId[sock], fileOwnerSocket)
-						break
-					}
+					fmt.Println("Sending request to file owner")
+					sendSocketMessage(REQUEST_CLIENT+FLAG_SEPARATOR+fileName+FLAG_SEPARATOR+socketToUsername[sock], fileOwnerSocket)
+
+				} else {
+					sendSocketMessage(ERROR+FLAG_SEPARATOR+"File owner is not online.", sock)
 				}
 
-				if !fileFound {
-					sendSocketMessage(ERROR+"@"+"File owner is not online.", sock)
-				}
+				// for key := range fileNames {
+
+				// 	if fileName == key {
+
+				// 		fileFound = true
+				// 		fileOwnerSocket := fileNames[key]
+				// 		fmt.Println("Sending request to file owner")
+				// 		sendSocketMessage(REQUEST_CLIENT+FLAG_SEPARATOR+fileName+FLAG_SEPARATOR+socketToUsername[sock], fileOwnerSocket)
+				// 		break
+				// 	}
+				// }
 
 			}
 		} else if header == SEND_TO_SERVER {
 
 			/// ========= Reading file from Sender
 			fileIsToSocket := sockets[strings.Trim(splitMessage[1], "\n")]
-			FileName := splitMessage[2]
-			FileName = strings.Trim(FileName, "\n")
 
-			str := strings.Trim(splitMessage[3], "\n")
+			if fileIsToSocket != nil {
 
-			strBytes := strings.Split(str, " ")
+				FileName := splitMessage[2]
+				FileName = strings.Trim(FileName, "\n")
 
-			var bytes []byte
+				str := strings.Trim(splitMessage[3], "\n")
 
-			for b := range strBytes {
-				newB, _ := strconv.Atoi(strBytes[b])
-				bytes = append(bytes, byte(newB))
+				strBytes := strings.Split(str, " ")
+
+				var bytes []byte
+
+				for b := range strBytes {
+					newB, _ := strconv.Atoi(strBytes[b])
+					bytes = append(bytes, byte(newB))
+				}
+
+				//  ======= Caching file
+
+				ioutil.WriteFile("./cache/"+FileName, bytes, 0666)
+
+				//  ======= Sending file
+				sendFile(FileName, bytes, fileIsToSocket)
+
 			}
-
-			//  ======= Caching file
-
-			ioutil.WriteFile("./cache/"+FileName, bytes, 0666)
-
-			//  ======= Sending file
-			sendFile(FileName, bytes, fileIsToSocket)
 
 		} else if header == ERROR {
 			errMessage := splitMessage[1]
@@ -122,22 +164,31 @@ func incomingHandler(sock net.Conn) {
 	}
 
 }
+
+func loginUser(username string, sock net.Conn) {
+	sockets[username] = sock
+	socketToUsername[sock] = username
+}
+
+func signupUser(username string, password string, sock net.Conn) {
+	userInfo[username] = password
+	file, _ := json.MarshalIndent(userInfo, "", " ")
+	ioutil.WriteFile(JSON_USER_INFO, file, 0644)
+	loginUser(username, sock)
+}
 func sendFile(fileName string, bytes []byte, fileIsToSocket net.Conn) {
 
 	strBytes := fmt.Sprintf("%v", bytes)
 	strBytes = strings.Trim(strBytes, "]")
 	strBytes = strings.Trim(strBytes, "[")
 	strBytes = strings.Trim(strBytes, " ")
-	sendSocketMessage(SENT_TO_CLIENT+"@"+fileName+"@"+strBytes, fileIsToSocket)
+	sendSocketMessage(SENT_TO_CLIENT+FLAG_SEPARATOR+fileName+FLAG_SEPARATOR+strBytes, fileIsToSocket)
 }
 
 func addFileName(fileName string, sock net.Conn) {
-	fileNames[strings.Trim(fileName, "\n")] = sock
 
-}
+	fileNames[strings.Trim(fileName, "\n")] = socketToUsername[sock]
 
-func addCacheFileName(fileName string) {
-	cachedFileNames = append(cachedFileNames, fileName)
 }
 
 func sendSocketMessage(message string, sock net.Conn) {
@@ -154,13 +205,8 @@ func cleanUp(sock net.Conn) {
 	fmt.Println("Socket is closed")
 	//TODO Send an error message to client
 
-	for fn := range fileNames {
-		if sock == fileNames[fn] {
-			delete(fileNames, fn)
-		}
-	}
-	delete(sockets, socketToId[sock])
-	delete(socketToId, sock)
+	delete(sockets, socketToUsername[sock])
+	delete(socketToUsername, sock)
 
 }
 
@@ -170,18 +216,24 @@ func acceptSockets() {
 	for {
 		sock, _ := l.Accept()
 		fmt.Println("Accepted")
-		time := time.Now()
-		sockets[time.String()] = sock
-		socketToId[sock] = time.String()
+
 		go incomingHandler(sock)
 	}
 }
 
 func main() {
+	userInfo = make(map[string]string)
 	sockets = make(map[string]net.Conn)
-	socketToId = make(map[net.Conn]string)
-	fileNames = make(map[string]net.Conn)
-	cachedFileNames = []string{}
+	socketToUsername = make(map[net.Conn]string)
+	fileNames = make(map[string]string)
+
+	bytes, error := ioutil.ReadFile(JSON_USER_INFO)
+
+	if error != nil {
+		fmt.Println("An error occured, couldn't get previously shared files.")
+	} else {
+		json.Unmarshal(bytes, &userInfo)
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
